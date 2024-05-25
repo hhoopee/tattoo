4ede34bd8ed31f014269055595b95ef25be356f2
hhoopee <vakylskij@gmail.com>
2024-05-25 22:28:51 +0300

    feat: add promo

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const navButton = document.querySelector('.nav-button');
 const navWrap = document.querySelector('.nav-wrap');
+const promoBg = document.querySelector('.promo-bg');
+
+window.addEventListener('load', () => {
+    promoBg.classList.add('active');
+})
 
 navButton.addEventListener('click', () => {
     navWrap.classList.toggle('active');
