040062291022656cf748361c9a448ef46fb1c967
hhoopee <vakylskij@gmail.com>
2024-05-27 23:02:41 +0300

    feat: add about and wwd

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const navButton = document.querySelector('.nav-button');
-const navWrap = document.querySelector('.nav-wrap');
-const promoBg = document.querySelector('.promo-bg');
+const navButton = document.querySelector(".nav-button");
+const navWrap = document.querySelector(".nav-wrap");
+const promoBg = document.querySelector(".promo-bg");
+const aboutSection = document.querySelector(".about-content__column-1");
 
-window.addEventListener('load', () => {
-    promoBg.classList.add('active');
-})
+const paralaxContainer1 = document.querySelector(".parallax-container-1");
+const paralaxContainer2 = document.querySelector(".parallax-container-2");
 
-navButton.addEventListener('click', () => {
-    navWrap.classList.toggle('active');
-    navButton.classList.toggle('active');
-})
+console.log(aboutSection);
+
+window.addEventListener("load", () => {
+  promoBg.classList.add("active");
+});
+
+navButton.addEventListener("click", () => {
+  navWrap.classList.toggle("active");
+  navButton.classList.toggle("active");
+});
+
+window.addEventListener("scroll", scroll);
+scroll();
+
+function scroll() {
+  const r = aboutSection.getBoundingClientRect();
+  const wHeight = window.innerHeight;
+
+  const paralaxYCenter = r.y + r.height / 2;
+  const scrollYCenter = window.innerHeight / 2;
+  
+
+  const move = (paralaxYCenter - scrollYCenter) * 0.16 - 100;
+
+
+  if (r.bottom > 0 && r.top < wHeight && r.top > 0) {
+    paralaxContainer1.style.transform = `translateY(${move/2}px)`;
+    paralaxContainer2.style.transform = `translateY(${-move/2}px)`;
+  }
+
+}
