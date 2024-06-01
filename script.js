1dae9b8c963180fb6980c4b97e5d1e831b64969d
hhoopee <vakylskij@gmail.com>
2024-06-01 09:21:32 +0300

    fix: fix sliders

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const aboutSection = document.querySelector(".about-content__column-1");
 const paralaxContainer1 = document.querySelector(".parallax-container-1");
 const paralaxContainer2 = document.querySelector(".parallax-container-2");
 
-
 window.addEventListener("load", () => {
   promoBg.classList.add("active");
 });
@@ -25,38 +24,53 @@ function scroll() {
 
   const paralaxYCenter = r.y + r.height / 2;
   const scrollYCenter = window.innerHeight / 2;
-  
 
   const move = (paralaxYCenter - scrollYCenter) * 0.16 - 100;
 
-
   if (r.bottom > 0 && r.top < wHeight && r.top > 0) {
-    paralaxContainer1.style.transform = `translateY(${move/2}px)`;
-    paralaxContainer2.style.transform = `translateY(${-move/2}px)`;
+    paralaxContainer1.style.transform = `translateY(${move / 2}px)`;
+    paralaxContainer2.style.transform = `translateY(${-move / 2}px)`;
   }
-
 }
 
-const slides = document.querySelectorAll('.gallery-slide');
+const slides = document.querySelectorAll(".gallery-slide");
 
-slides.forEach(slide => {
+slides.forEach((slide) => {
   lightGallery(slide, {
     download: false,
+    counter: false,
   });
-})
+});
 
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    loop: true,
-    smartSpeed: 2000,
-    autoplayTimeout:5000,
-    autoplayHoverPause: true,
-    autoplay: true,
-    nav: true,
-    dots: false,
-    items: 3,
-    navClass: ["owl-prev", "owl-next"],
-    navText: ["<i class='fa-solid fa-arrow-left'></i>", "<i class='fa-solid fa-arrow-right'></i>"],
+const carousels = document.querySelectorAll(".owl-carousel");
+
+carousels.forEach((carousel) => {
+  const dataLoop = carousel.getAttribute("data-loop");
+  const dataAutoplay = carousel.getAttribute("data-autoplay");
+  const dataLgItems = carousel.getAttribute("data-lg-items");
+  const dataMdItems = carousel.getAttribute("data-md-items");
+  const dataSmItems = carousel.getAttribute("data-sm-items");
+  const dataSmartSpeed = carousel.getAttribute("data-smart-speed");
+  const dataAutoplayTimeout = carousel.getAttribute("data-autoplay-timeout");
+  const dataAutoplayHoverPause = carousel.getAttribute("data-autoplay-hoverpause");
+  const dataMouseDrag = carousel.getAttribute("data-mouse-drag");
+  const dataNav = carousel.getAttribute("data-nav");
+  const dataDots = carousel.getAttribute("data-dots");
+
+  $(carousel).owlCarousel({
+    autoplay: dataAutoplay === "true" ? true : false,
+    loop: dataLoop === "true" ? true : false,
+    items: dataLgItems,
+    smartSpeed: +dataSmartSpeed,
+    autoplayTimeout: +dataAutoplayTimeout,
+    autoplayHoverPause: dataAutoplayHoverPause === "true" ? true : false,
+    mouseDrag: dataMouseDrag === "true" ? true : false,
+    nav: dataNav === "true" ? true : false,
+    dots: dataDots === "true" ? true : false,
+    navText: [
+      "<i class='fa-solid fa-arrow-left'></i>",
+      "<i class='fa-solid fa-arrow-right'></i>",
+    ],
   });
 });
 
